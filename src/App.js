5a6f3ea59a2f77586b1817724d56fdf71626a792
Jithin Sathyan <jithinsathyan0709@gmail.com>
2020-06-07 21:30:38 +0530

    functional component with states using hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { ThemeProvider } from 'styled-components';
+
+import "antd/dist/antd.css";
+
+import NormalParentComponent from "./content/NormalComponent";
+
+//Theming fonts
+import fontTheme from './utils/fontTheme';
+import GlobalStyles from './utils/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={fontTheme}>
+      <Router>
+        <Suspense fallback="loading">
+          <Switch>
+            {/* <Route exact path="/redux-heirarchy" component={LoginComponent} /> */}
+            <Route exact path="/normal-heirarchy" component={NormalParentComponent} />
+            <Route path="/" exact render={() => <Redirect to="/normal-heirarchy" />} />
+          </Switch>
+        </Suspense>
+      </Router>
+      <GlobalStyles />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/ListingComponent.jsx
@@ -0,0 +1,92 @@
+//import packages
+import React from "react";
+import { CheckCircleFilled } from "@ant-design/icons";
+import { Row, Col, Typography, Card, Layout } from "antd";
+import styled from "styled-components";
+
+//import images
+
+const { Title, Text } = Typography;
+const { Header } = Layout;
+
+const StyledHeader = styled(Header)`
+  background-color: #49548d;
+  color: #ffffff;
+  padding: 8px 12px;
+`;
+
+const StyledTitle = styled(Title)`
+  color: #ffffff !important;
+`;
+
+const StyledDiv = styled.div`
+  border: 1px solid #49548d;
+  border-radius: 4px;
+  margin: 20px 28px;
+`;
+
+const StyledNoContentTitle = styled(Title)`
+  color: #8c8585 !important;
+`;
+
+const StyledNoContentWrapper = styled(Col)`
+  text-align: center;
+  padding: 40px 0;
+`;
+
+const DictionaryComponent = ({ title, list, updateList }) => {
+  const onCardClick = (eachItem) => {
+    if (updateList) {
+      updateList(eachItem);
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <StyledDiv>
+      <StyledHeader>
+        <StyledTitle level={4}>{title}</StyledTitle>
+      </StyledHeader>
+      <Row>
+        {list.length ? (
+          list.map((eachCard, index) => {
+            return (
+              <Col
+                key={`${eachCard.id}-key-${index}`}
+                xs={6}
+                sm={6}
+                md={6}
+                lg={6}
+                xl={6}
+                style={{ padding: "12px" }}
+              >
+                <Card
+                  hoverable
+                  key={eachCard.id}
+                  onClick={() => {
+                    onCardClick(eachCard);
+                  }}
+                  title={eachCard.title}
+                  extra={
+                    eachCard.selected ? <CheckCircleFilled /> : <span></span>
+                  }
+                >
+                  <Text>{eachCard.description}</Text>
+                </Card>
+              </Col>
+            );
+          })
+        ) : (
+          <StyledNoContentWrapper span={24}>
+            <StyledNoContentTitle level={4}>
+              No items to show
+            </StyledNoContentTitle>
+          </StyledNoContentWrapper>
+        )}
+      </Row>
+    </StyledDiv>
+  );
+};
+
+export default DictionaryComponent;
